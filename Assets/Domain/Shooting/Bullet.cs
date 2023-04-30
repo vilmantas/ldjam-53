6709457fefce7ffde9f3ae809571e4c64b874788
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2023-04-30 20:52:36 +0300

    Add Firing script to bunkers

---
+++ Assets/Domain/Shooting/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float MinBulletSpeed = 30;
+    public float currentSpeed;
+    public AudioSource audio;
+    public float AudioFrequency = 30;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentSpeed = rigidbody.velocity.magnitude;
+        if ( rigidbody.velocity.magnitude < MinBulletSpeed)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Random.Range(0,100)<= AudioFrequency)
+        {
+            //audio.Play();
+        }
+    }
+}
---
+++ Assets/Domain/Shooting/Firing.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public GameObject bullet;
+    public float variance = 0.1f;
+    public float gunSpeed = 0.2f;
+    public float magazineSize = 10;
+    public float reloadTime = 3;
+    public float bulletSpeed = 5;
+    AudioSource audio;
+    public Transform BulletSpawn;
+
+    void Start()
+    {
+        //audio = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        InvokeRepeating("FireMagazine", 0, reloadTime);
+    }
+
+    void FireMagazine()
+    {
+        //audio.Play();
+        for (int n = 0; n< magazineSize; n++)
+        {
+            Invoke("Fire", n* gunSpeed);
+        }
+        Invoke("stopSound", magazineSize * gunSpeed);
+    }
+
+    // Update is called once per frame
+    void Fire()
+    {
+        var instance = Instantiate(bullet);
+        instance.transform.position = BulletSpawn.position;
+        var body = instance.GetComponent<Rigidbody>();
+        var target = new Vector3( PlayerTransform.position.x + Random.Range(-1*variance,variance),
+            PlayerTransform.position.y + Random.Range(-1 * variance/2, variance/2),
+            PlayerTransform.position.z + Random.Range(-1 * variance, variance)
+        );
+
+        body.velocity =  (target - transform.position)* bulletSpeed;
+        Destroy(instance, 3);
+    }
+    void stopSound()
+    {
+        //audio.Stop();
+    }
+}
